34c0bd58c891c7f02a6f979ea6204a2ad977ba45
Andrey Semka <k9nz00@yandex.ru>
2020-06-18 21:55:45 +0300

    Модуль 8 задание 2 - оповещения выводятся при помощи библеотеки Bootstrap notify

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,6 +1,12 @@
 window.Echo
     .private('admin')
     .listen('UpdatePost', (event) => {
-        window.alert(event.message);
-        window.alert(event.linkToPost);
+        $.notify({
+            icon: 'glyphicon glyphicon-warning-sign',
+            title: 'Статья ' + event.post.title + ' была изменена.',
+            message: event.message,
+            url: event.linkToPost
+        }, {
+            type: "success",
+        });
     });
